24e5e6b2c31a53319a4be9380be3a776fc3de1fa
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-05-11 09:57:48 +0600

    up to date

--- Admin/database/dbConnection.php
+++ Admin/database/dbConnection.php
@@ -1,8 +1,14 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database_name = "easy_commerce";
+
+// $servername = "localhost";
+// $username = "easytechx";
+// $password = "_^Mlr+NnZ=ga";
+// $database_name = "easytechx_easy_commerce";
 
 $conn = mysqli_connect($servername, $username, $password, $database_name);
 
@@ -10,4 +16,5 @@ $conn = mysqli_connect($servername, $username, $password, $database_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+
 ?>
