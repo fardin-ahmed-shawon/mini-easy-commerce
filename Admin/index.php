525a6a9a7245b5dd55a2f3e8105de66f19bc5495
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-05-11 21:51:26 +0600

    up to date

--- Admin/index.php
+++ Admin/index.php
@@ -70,7 +70,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['accept_order'])) {
             </div>
 
             <div class="row">
-              <div class="col-md-3 stretch-card grid-margin">
+              <div class="col-md-4 stretch-card grid-margin">
                 <div class="card bg-gradient-danger card-img-holder text-white">
                   <div class="card-body">
                     <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
@@ -88,26 +88,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['accept_order'])) {
                   </div>
                 </div>
               </div>
-              <div class="col-md-3 stretch-card grid-margin">
-                <div class="card bg-gradient-info card-img-holder text-white">
-                  <div class="card-body">
-                    <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-                    <h4 class="font-weight-normal mb-3">Customers 
-                      <i class="mdi mdi-account mdi-24px float-end"></i>
-                    </h4>
-                    <h1 class="mb-5">
-                      <?php
-                      // Fetch total customers from user_info table
-                      $sql = "SELECT COUNT(user_id) AS total_customers FROM user_info";
-                      $result = $conn->query($sql);
-                      $row = $result->fetch_assoc();
-                      echo $row['total_customers'];
-                      ?>
-                    </h1>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-3 stretch-card grid-margin">
+              <div class="col-md-4 stretch-card grid-margin">
                 <div class="card bg-gradient-success card-img-holder text-white">
                   <div class="card-body">
                     <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
@@ -125,7 +106,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['accept_order'])) {
                   </div>
                 </div>
               </div>
-              <div class="col-md-3 stretch-card grid-margin">
+              <div class="col-md-4 stretch-card grid-margin">
                 <div class="card bg-gradient-primary card-img-holder text-white">
                   <div class="card-body">
                     <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
